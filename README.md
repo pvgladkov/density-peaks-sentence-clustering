81a6dc41a3b0e40ccf32f5fc700acdeb7a042e1e
Paul Gladkov <gladkov.p@gmail.com>
2020-11-28 16:37:23 +0000

    baselines

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# density-peaks-sentence-clustering
-Density Peaks Sentence Clustering
+# Density Peaks Sentence Clustering
+
+[Clustering Sentences with Density Peaks for Multi-document Summarization](https://www.aclweb.org/anthology/N15-1136/)
+
+
+## CNN/Daily Mail
+
+|Model|ROUGE-1|ROUGE-2|ROUGE-L|
+|---|---|---|---|
+|Lead 75|13.13|3.85|13.25|
+|Lead 3 sent.|31.95|13.01|30.45|
+|DPSC||||
+
+## Gigaword
+
+|Model|ROUGE-1|ROUGE-2|ROUGE-L|
+|---|---|---|---|
+|Lead 75|15.38|4.43|14.71|
+|Lead 3 sent.|15.58|4.45|14.84|
+|DPSC||||
+
+## Run tests
+
+```bash
+pytest tests
+```
---
+++ experiments/baselines.py
@@ -0,0 +1,42 @@
+import tensorflow_datasets as tfds
+from nltk.tokenize import sent_tokenize
+
+from rouge import Rouge
+
+
+def lead_75(t):
+    return t[:75]
+
+
+def lead_3s(t):
+    sentences = [s for s in sent_tokenize(t)]
+    return '. '.join(sentences[:3])
+
+
+if __name__ == '__main__':
+
+    datasets = {
+        'cnn_dailymail': tfds.load('cnn_dailymail', split='test', as_supervised=True, shuffle_files=False),
+        'gigaword': tfds.load('gigaword', split='test', as_supervised=True, shuffle_files=False),
+    }
+
+    for name, dataset in datasets.items():
+        hyps_75 = []
+        hyps_3s = []
+        refs = []
+
+        for text, ref in dataset:
+
+            text = text.numpy()
+            hyps_75.append(lead_75(str(text)))
+            hyps_3s.append(lead_3s(str(text)))
+
+            ref = ref.numpy()
+            refs.append(str(ref))
+
+        metric = Rouge()
+        score_75 = metric.get_scores(hyps_75, refs, avg=True)
+        score_3s = metric.get_scores(hyps_3s, refs, avg=True)
+        print(name)
+        print(f'lead 75: {score_75}')
+        print(f'lead 3s: {score_3s}')
